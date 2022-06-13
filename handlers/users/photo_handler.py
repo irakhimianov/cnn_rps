7929c44c07cd15563a317ad1df55286aca9484fa
rkjar-work <rkjar@yandex.ru>
2022-06-13 15:44:43 +0500

    Initial commit

--- handlers/users/photo_handler.py
+++ handlers/users/photo_handler.py
@@ -1,7 +1,7 @@
 from aiogram import types
 # from keyboards.inline import
 from loader import dp, bot
-from util import get_result
+from util import get_result, make_result_img
 from models import RPS, RPS_EMOJI
 from cnn import get_prediction
 
@@ -14,9 +14,17 @@ async def user_photo(message: types.Message):
                                 f'результат бота: {my_result[0]} {my_result[1]}',
                            chat_id=message.from_user.id)
     predict = get_prediction('static/temp.jpg')
-    await bot.send_message(text=f'получен ответ\n'
-                                f'результат: {predict}',
-                           chat_id=message.from_user.id)
+    cool_img = make_result_img(RPS_EMOJI.get(predict),
+                               RPS_EMOJI.get(my_result[0]))
+    # await bot.send_message(text=f'получен ответ\n'
+    #                             f'результат: {predict}',
+    #                        chat_id=message.from_user.id)
+
+    await bot.send_photo(chat_id=message.from_user.id,
+                         photo=cool_img,
+                         caption=f'получен ответ\n'
+                                 f'результат: {predict}')
+
 
 async def send_game_result():
     bot_result = get_result()
--- util/__init__.py
+++ util/__init__.py
@@ -1,3 +1,4 @@
 from .bot_default_commands import set_default_commands
 from .bot_notify_admin import on_startup_notify
 from .bots_game_result import get_result
+from .cool_img_maker import make_result_img
