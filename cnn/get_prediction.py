38fa458fa7e3f8bf716a6bb9a20135e47f15d054
rkjar-work <rkjar@yandex.ru>
2022-06-13 15:29:57 +0500

    Initial commit

--- cnn/get_prediction.py
+++ cnn/get_prediction.py
@@ -1,24 +1,24 @@
 import tensorflow as tf
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+# import matplotlib.pyplot as plt
+# import matplotlib.image as mpimg
 from keras_preprocessing import image
-from keras_preprocessing.image import ImageDataGenerator
+# from keras_preprocessing.image import ImageDataGenerator
 
 
-model = tf.keras.models.load_model('model.h5')
-print(model.summary())
+model = tf.keras.models.load_model('static/model.h5')
 name_classes = ['Бумага', 'Камень', 'Ножницы']
 
-img_path = 'test.png'
-img = image.load_img(img_path, target_size=(150, 150))
-x = image.img_to_array(img)
-x = np.expand_dims(x, axis=0)
-images = np.vstack([x])
-classes = model.predict(images, batch_size=10)
-print(classes)
-print(name_classes[np.argmax(classes)])
-img = mpimg.imread(img_path)
-plt.imshow(img)
-plt.axis('Off')
-plt.show()
+
+def get_prediction(img_path: str) -> str:
+    img = image.load_img(img_path, target_size=(150, 150))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    images = np.vstack([x])
+    classes = model.predict(images, batch_size=10)
+
+    return name_classes[np.argmax(classes)]
+    # img = mpimg.imread(img_path)
+    # plt.imshow(img)
+    # plt.axis('Off')
+    # plt.show()
--- handlers/users/photo_handler.py
+++ handlers/users/photo_handler.py
@@ -3,15 +3,20 @@ from aiogram import types
 from loader import dp, bot
 from util import get_result
 from models import RPS, RPS_EMOJI
+from cnn import get_prediction
 
 
 @dp.message_handler(content_types=types.ContentType.PHOTO)
 async def user_photo(message: types.Message):
     my_result = await send_game_result()
     await message.photo[-1].download(destination_file='static/temp.jpg')
-    await bot.send_message(text=f'получена картинка: {my_result[0]} {my_result[1]}',
+    await bot.send_message(text=f'получена картинка:\n'
+                                f'результат бота: {my_result[0]} {my_result[1]}',
                            chat_id=message.from_user.id)
-
+    predict = get_prediction('static/temp.jpg')
+    await bot.send_message(text=f'получен ответ\n'
+                                f'результат: {predict}',
+                           chat_id=message.from_user.id)
 
 async def send_game_result():
     bot_result = get_result()
